77688d8ed750fe5f393781c91800cc4d498b356c
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-11 17:42:35 +0530

    code updated

---
+++ android/build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+}
+
+group = "com.sharmadhiraj.installed_apps"
+version = "1.0-SNAPSHOT"
+
+repositories {
+        google()
+        mavenCentral()
+    }
+
+android {
+    namespace = "com.sharmadhiraj.installed_apps"
+
+    compileSdk = 37
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+        getByName("test") {
+            java.srcDirs("src/test/kotlin")
+        }
+    }
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+}
